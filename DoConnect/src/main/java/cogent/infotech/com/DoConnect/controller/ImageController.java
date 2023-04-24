9d193a8a4d09a5451f174f8ddaf6f6431a921e0d
alnidu1 <73094665+alnidu1@users.noreply.github.com>
2023-04-24 15:29:48 -0500

    admin crud

--- DoConnect/src/main/java/cogent/infotech/com/DoConnect/controller/ImageController.java
+++ DoConnect/src/main/java/cogent/infotech/com/DoConnect/controller/ImageController.java
@@ -5,29 +5,64 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class ImageController {
 
-    @PostMapping("/upload")
-    public ResponseEntity<?> uploadImage(@RequestParam("file")MultipartFile file) {
-        try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get("/path/to/uploaded/files/"+ file.getOriginalFilename());
-            Files.write(path, bytes);
+	@Value("${upload.path}")
+	private String uploadPath;
 
-            String fileUrl = "http://localhost:8080/api/files/" + file.getOriginalFilename();
-            return ResponseEntity.ok().body(fileUrl);
+
+    @PostMapping("/image/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+            String fileExtension = FilenameUtils.getExtension(fileName);
+
+            if (!Arrays.asList("jpg", "jpeg", "png").contains(fileExtension.toLowerCase())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid file format");
+            }
+
+            String uniqueFileName = UUID.randomUUID().toString() + "." + fileExtension;
+
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs();
+            }
+
+            Path path = Paths.get(uploadPath + File.separator + uniqueFileName);
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+            return ResponseEntity.ok("File uploaded successfully");
         } catch (IOException e) {
             e.printStackTrace();
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading file");
         }
     }
 }
