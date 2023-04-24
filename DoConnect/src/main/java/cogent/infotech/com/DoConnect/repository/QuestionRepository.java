e2b024efd71499db2e06a2a57d310ca313c3266d
alnidu1 <73094665+alnidu1@users.noreply.github.com>
2023-04-24 14:09:39 -0500

    a

--- DoConnect/src/main/java/cogent/infotech/com/DoConnect/repository/QuestionRepository.java
+++ DoConnect/src/main/java/cogent/infotech/com/DoConnect/repository/QuestionRepository.java
@@ -1,9 +1,11 @@
 package cogent.infotech.com.DoConnect.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cogent.infotech.com.DoConnect.entity.Question;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer>{
 
 }
--- DoConnect/src/main/java/cogent/infotech/com/DoConnect/repository/UserRepository.java
+++ DoConnect/src/main/java/cogent/infotech/com/DoConnect/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package cogent.infotech.com.DoConnect.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cogent.infotech.com.DoConnect.entity.User;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-	User findByUserName(String username);
+	User findByUsername(String username);
 }
