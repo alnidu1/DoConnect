34921fe1c42282c55d35179debe24164f3c2c9e7
alnidu1 <73094665+alnidu1@users.noreply.github.com>
2023-04-26 09:25:59 -0500

    add user frontend register

--- DoConnect/src/main/java/cogent/infotech/com/DoConnect/controller/UserController.java
+++ DoConnect/src/main/java/cogent/infotech/com/DoConnect/controller/UserController.java
@@ -6,24 +6,27 @@ import cogent.infotech.com.DoConnect.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cogent.infotech.com.DoConnect.entity.User;
 
 
-
+@CrossOrigin
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
 
 	@Autowired
 	private UserService service;
 	
-	@GetMapping("/")
+	@GetMapping("")
 	public String home() {
 		return "Welcome to DoConnect";
 	}
--- DoConnectFrontEnd/src/app/app-routing.module.ts
+++ DoConnectFrontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminRegistrationComponent } from './admin-registration/admin-registration.component';
 import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user-component/user-component.component';
 
 const routes: Routes = [
   {path:"adminlogin", component: LoginComponent},
-  {path:"adminregister", component: AdminRegistrationComponent}
+  {path:"adminregister", component: AdminRegistrationComponent},
+  {path:"user", component:UserComponent}
 ];
 
 @NgModule({
