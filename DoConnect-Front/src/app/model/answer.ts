790b5be413b2b6b6345b4275c7e48d456b4021e9
Grecidufer28 <Anudeepg28@gmail.com>
2023-05-08 01:59:46 -0500

    Fixed answer image bug and fixed some formatting.

--- DoConnect-Front/src/app/model/answer.ts
+++ DoConnect-Front/src/app/model/answer.ts
@@ -4,7 +4,7 @@ export class Answer {
     constructor(
         public id: number,
         public description_answer: string,
-        public image_src: string,
+        public img_src: string,
         public status: string,
         public datetime: string | null,
         public question_id:number,
